dcab9a6b65e6aa140588368103c6e936075c259a
nabsty55 <nabsty55@gmail.com>
2019-09-16 09:15:25 +0300

    the grades object is working

--- grades-object.js
+++ grades-object.js
@@ -4,17 +4,19 @@ let Grades = function(grade1, grade2, grade3, grade4){
   this.biology = grade3;
   this.math = grade4;
   this.total = function(physics, chemistry, biology, math){
-    let gradeTotal = physics + chemistry  + biology + math;
+    let gradeTotal = this.physics + this.chemistry  + this.biology + this.math;
     return gradeTotal;
   }
   this.average =function(physics, chemistry, biology, math){
-    let gradeAverage = total()/4;
-    console.log("Students grade average is " + gradeAverage);
+    let gradeAverage = this.total()/4;
+    console.log("Student's grade average is " + gradeAverage);
   }
 
 }
 
 let student1 =new Grades(50, 60, 75, 80);
 console.log(student1);
+console.log(student1.average());
 let student2 = new Grades(75, 95, 85, 50);
 console.log(student2);
+console.log(student2.average());
